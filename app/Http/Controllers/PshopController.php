11219c98800a284fd7964f7ea3fa7942648833fb
kazemi.milad <kazemi.milad@gmail.com>
2025-03-29 17:18:14 +0330

    update

--- app/Http/Controllers/PshopController.php
+++ app/Http/Controllers/PshopController.php
@@ -607,7 +607,7 @@ class PshopController extends Controller
                 }
             }
 
-            $holooController = new HolooController();
+            $holooController = resolve(\App\Http\Controllers\HolooController::class);
             $holooProduct = $holooController->GetMultiProductHoloo($HolooIDs);
 
             if (!isset(json_decode($holooProduct)->data->product)) {
