7ce0497d4d8c7b6ca2f1b1a80c68034a456bfab8
kazemi.milad <kazemi.milad@gmail.com>
2025-04-06 01:21:01 +0330

    update

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DownloadController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/exportInvoicesLastWeek', [DownloadController::class, 'exportInvoicesLastWeek'])->name("exportInvoicesLastWeek");
